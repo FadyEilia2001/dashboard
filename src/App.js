84e315cd103a4c677e52bdc69abb749dd99fdd00
FadyEilia2001 <feilia@WGU.edu>
2022-08-06 14:35:41 -0500

    Completed Project structure
    Added pages/charts/data/components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
-
+import {Navbar, Footer, Sidebar, ThemeSettings } from './components'
+import { Ecommerce, Orders, Calendar, EMployees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Employees, Line } from './pages'
 import './App.css'
 
 const App = () => {
-    const activeMenu = true
+    const activeMenu = false
   return (
     <div>
         <BrowserRouter>
-            <div className='flex relative dark:bg-main-dark-bg'>
+            
+            {/* Main div spanning the whole page */}
+            <div className='flex relative dark:bg-main-dark-bg'> 
+
+                {/* Settings icon bottom right inside of a div */}
                 <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
                     <TooltipComponent content="Settings" position='Top'>
                         <button type='button' 
@@ -22,34 +27,60 @@ const App = () => {
                         </button>
                     </TooltipComponent>
                 </div>
+                
+                {/* Side bar */}
                 {activeMenu ? (
                     <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-                        Sidebar
+                        <Sidebar />
                     </div>
                 ) : (
                     <div className='w-0 dark:bg-secondary-dark-bg'>
-                        Sidebar
+                        <Sidebar />
                     </div>
                 )}
 
+                {/* Navbar next to side bar, and it changes based on the sidebar appearnce*/}
                 <div className={
                     `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                 }>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                        navbar
+                        <Navbar />
                     </div>
                 </div>
-
+                
+                {/* Routes on main page {div} */}
                 <div>
                     <Routes>
-                        {/* Dashboard */}
-                        <Route path="/" element="ECommerce" />
-                        <Route path="/ecommerce" element="ECommerce" />
+                        {/* Dashboard initial page*/}
+                        <Route path="/" element={<Ecommerce />}/>
+                        <Route path="/ecommerce" element={<Ecommerce />} />
                         
                         {/* Pages */}
+                        <Route path="/orders" element={<Orders />}/>
+                        <Route path="/employes" element={<Employees />} />
+                        <Route path="/customers" element={<Customers />} />
+
+                        {/* Apps routes */}
+                        <Route path="/kanban" element={<Kanban />} />
+                        <Route path="/editor" element={<Editor />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/color-picker" element={<ColorPicker />} />
+
+
+                        {/* Charts routes */}
+                        <Route path="/line" element={<Line />} />
+                        <Route path="/area" element={<Area />} />
+                        <Route path="/bar" element={<Bar />} />
+                        <Route path="/pie" element={<Pie />} />
+                        <Route path="/financial" element={<Financial />} />
+                        <Route path="/color-mapping" element={<ColorMapping />} />
+                        <Route path="/pyramid" element={<Pyramid />} />
+                        <Route path="/stacked" element={<Stacked />} />
                         
                     </Routes>
                 </div>
+
+
             </div>
             
         </BrowserRouter>

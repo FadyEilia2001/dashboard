030194c3f732d2be24c7c24e665d532b8c5bcc43
FadyEilia2001 <feilia@WGU.edu>
2022-08-07 23:13:53 -0500

    Completed ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -95,7 +95,25 @@ const Ecommerce = () => {
                 <SparkLine currentColor='blue' id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color='blue' />
               </div>
 
+              <div className='mt-10'>
+                <Button 
+                  color='white' 
+                  bgColor='blue'
+                  text='Download Report'
+                  borderRadius='10px'
+                />
+              </div>
+
             </div>
+
+            <div>
+              <Stacked 
+                width="320px" 
+                height="360px"
+              />
+            </div>
+
+
           </div>
 
         </div>

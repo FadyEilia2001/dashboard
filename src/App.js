d95e3d4c104c02ca92589f338fe5896c2dac8128
FadyEilia2001 <feilia@WGU.edu>
2022-08-08 10:02:36 -0500

    Completed Employee table with Search

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
                         
                         {/* Pages */}
                         <Route path="/orders" element={<Orders />}/>
-                        <Route path="/employes" element={<Employees />} />
+                        <Route path="/employees" element={<Employees />} />
                         <Route path="/customers" element={<Customers />} />
 
                         {/* Apps routes */}
